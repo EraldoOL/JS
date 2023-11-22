6e565ddee1abaecbcb6a09ea3bcfb73c08e91162
eo80797@gmail.com <eo80797@gmail.com>
2023-11-22 09:33:44 -0300

    tt

--- exercícios/script.js
+++ exercícios/script.js
@@ -3,7 +3,6 @@ function carregar(){
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
-var hora = 22
 msg.innerHTML = `Agora são ${hora} horas`
 if(hora >= 0 && hora < 12){
   //Bom dia
